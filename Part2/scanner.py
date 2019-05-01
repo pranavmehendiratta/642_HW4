1253ecd746ea90906ff242cce4e45c34d18d52e0
Pranav Mehendiratta <pranav.mehendiratta@gmail.com>
2019-04-30 22:21:04 -0500

    Renamed the dictionary for arp spoofing devices

--- Part2/scanner.py
+++ Part2/scanner.py
@@ -3,15 +3,14 @@ import sys
 import binascii
 import socket
 
+arp_spoofing_devices = dict()
+arp_spoofing_devices["192.168.0.100"] = "7c:d1:c3:94:9e:b8"
+arp_spoofing_devices["192.168.0.103"] = "d8:96:95:01:a5:c9"
+arp_spoofing_devices["192.168.0.1"] = "f8:1a:67:cd:57:6e"
 
-device = dict()
-device["192.168.0.100"] = "7c:d1:c3:94:9e:b8"
-device["192.168.0.103"] = "d8:96:95:01:a5:c9"
-device["192.168.0.1"] = "f8:1a:67:cd:57:6e"
-
-device["7c:d1:c3:94:9e:b8"] = "192.168.0.100"
-device["d8:96:95:01:a5:c9"] = "192.168.0.103"
-device["f8:1a:67:cd:57:6e"] = "192.168.0.1"
+arp_spoofing_devices["7c:d1:c3:94:9e:b8"] = "192.168.0.100"
+arp_spoofing_devices["d8:96:95:01:a5:c9"] = "192.168.0.103"
+arp_spoofing_devices["f8:1a:67:cd:57:6e"] = "192.168.0.1"
 
 def add_colons_to_mac(mac_addr):
     """This function accepts a 12 hex digit string and converts it to a colon
@@ -39,11 +38,14 @@ def detectArpSpoofing(filename):
                 src_mac = add_colons_to_mac(binascii.hexlify(arp.sha))
                 #dst_mac = add_colons_to_mac(binascii.hexlify(arp.tha))
 
-                ip_for_src_mac = device[src_mac]
+                ip_for_src_mac = arp_spoofing_devices[src_mac]
                 if ip_for_src_mac != src_ip:
                     print "ARP spoofing!"
                     print "MAC:", src_mac
                     print "Packet number:", frame_counter
+
+
+
 
 if __name__ == '__main__':
     detectArpSpoofing(sys.argv[1])
